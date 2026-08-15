0c5c6741edd4cf9b92a658ced732a67972deb171
ozawa <ozawakitto0103.ex@gmail.com>
2026-08-15 17:32:17 +0900

    物件詳細ページのUIを拡充し、テンプレート誤上書きを修正
    - Propertyに最寄り駅/距離/築年数フィールドを追加
    - 詳細ページに画像プレースホルダー・情報テーブル・問い合わせボタンを追加
    - footerを画面下部に固定表示
    - property.htmlが詳細ページ内容で上書きされていた問題を修正

---
+++ src/main/java/com/example/demoweb/Property.java
@@ -0,0 +1,19 @@
+package com.example.demoweb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Property {
+    private int id;
+    private String name;
+    private String price;
+    private String location;
+    private String layout;
+    private String nearestStation;
+    private String access;
+    private String age;
+}
